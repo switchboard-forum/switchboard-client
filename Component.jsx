4b447081fc70834e0a49148d7d7216fdec416b92
maxellnormalbias <0087yugbocaj@gmail.com>
2017-02-13 20:28:17 -0800

    When one writes commit messages, they are merely projecting the contents of their ego onto a purely mechanical device - the mechanical device, you could say then, has become a partial clone, containing a small element of their ego.

---
+++ Component.jsx
@@ -0,0 +1,17 @@
+let React = require("react");
+
+module.exports = React.createClass({
+  render() {
+    return (
+      <html>
+        <head>
+          <title>Switchboard Prototype</title>
+        </head>
+        <body>
+          <h1>Switchboard Prototype</h1>
+          <hr/>
+        </body>
+      </html>
+    );
+  }
+});
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-let http = require("http");
-let net = require("net");
+require("babel-register")({
+  presets: ["react"]
+});
 
-var client = new net.Socket();
+let express = require("express");
+let net = require("net");
+let React = require("react");
+let ReactDOMServer = require("react-dom/server");
+
+let config = require("./config.json");
+let Component = require("./Component.jsx");
+
+let client = new net.Socket();
+
 client.connect(31337, '127.0.0.1', () => {
-	console.log('Connected');
+  console.log('Connected');
 });
 
 client.on('data', data => {
-	console.log('Received: ' + data);
+  console.log('Received: ' + data);
 });
 
 client.on('close', () => {
-	console.log('Connection closed');
+  console.log('Connection closed');
 });
 
+let server = express();
+
+server.get('/', (request, response) => {
+  const html = ReactDOMServer.renderToString(
+    React.createElement(Component)
+  );
+  response.send(html);
+});
+
+server.listen(8086);
